7d528d57999fd354edfa19a31afb7bcb75f74113
Seosangbeom <tjtkdqja1215@naver.com>
2026-04-04 14:41:12 +0900

    feat:springboot setting

---
+++ src/main/java/com/mbsystem/MBSystem/MbSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mbsystem.MBSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbSystemApplication.class, args);
+	}
+
+}
