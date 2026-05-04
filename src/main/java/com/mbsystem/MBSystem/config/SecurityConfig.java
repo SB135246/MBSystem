2313b13cc2ae6d36ea83cee74d4481fe1f6cc9b1
Seosangbeom <tjtkdqja1215@naver.com>
2026-05-04 21:02:26 +0900

    feat:Security Config 추가

---
+++ src/main/java/com/mbsystem/MBSystem/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.mbsystem.MBSystem.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/**").permitAll()   // ESP32 허용
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+}
