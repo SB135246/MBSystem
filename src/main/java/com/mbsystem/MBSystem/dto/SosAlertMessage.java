619f555e979fab55aa53fc6e3eb484df692c32d8
rororo33 <sjznsjx25792@gmail.com>
2026-05-14 19:20:28 +0900

    fix : SosAlertMessage 수정

--- src/main/java/com/mbsystem/MBSystem/dto/SosAlertMessage.java
+++ src/main/java/com/mbsystem/MBSystem/dto/SosAlertMessage.java
@@ -1,5 +1,6 @@
 package com.mbsystem.MBSystem.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -11,7 +12,9 @@ public class SosAlertMessage {
     private Long sosId;
     private Long moduleNum;
     private Long placeId;
+    @JsonProperty("xCordinate")
     private Double xCordinate;
+    @JsonProperty("yCordinate")
     private Double yCordinate;
     private Instant sosAt;
 }
