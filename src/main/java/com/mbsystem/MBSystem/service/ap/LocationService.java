0afa223953256b4f5a22f3b96b79634b0b83a47f
TaekMin LEE <uh06056@gmail.com>
2026-05-25 17:44:45 +0900

    fix: Location Service 불필요한 코드 제거

--- src/main/java/com/mbsystem/MBSystem/service/ap/LocationService.java
+++ src/main/java/com/mbsystem/MBSystem/service/ap/LocationService.java
@@ -126,43 +126,4 @@ public class LocationService {
     return 6.0;
   }
 
-  /**
-   * RSSI를 거리(m)로 변환하는 공식 (Log-Distance Path Loss Model)
-   */
-  private double rssiToDistance(double rssi) {
-    double distance =
-        Math.pow(
-            10,
-            (TX_POWER_1M - rssi)
-                / (10 * N_CONSTANT)
-        );
-
-    // 최대 거리 제한
-    return Math.min(distance, 12.0);
-  }
-
-  /**
-   * 삼변측량 수식 (Trilateration)
-   * p1, p2, p3는 각각 {x, y, d} 형태의 배열
-   */
-  private double[] trilateration(double[] p1, double[] p2, double[] p3) {
-    double x1 = p1[0], y1 = p1[1], d1 = p1[2];
-    double x2 = p2[0], y2 = p2[1], d2 = p2[2];
-    double x3 = p3[0], y3 = p3[1], d3 = p3[2];
-
-    // 수식 단순화를 위한 선형 방정식 풀이
-    double A = 2 * (x2 - x1);
-    double B = 2 * (y2 - y1);
-    double C = Math.pow(d1, 2) - Math.pow(d2, 2) - Math.pow(x1, 2) + Math.pow(x2, 2) - Math.pow(y1, 2) + Math.pow(y2, 2);
-
-    double D = 2 * (x3 - x2);
-    double E = 2 * (y3 - y2);
-    double F = Math.pow(d2, 2) - Math.pow(d3, 2) - Math.pow(x2, 2) + Math.pow(x3, 2) - Math.pow(y2, 2) + Math.pow(y3, 2);
-
-    // Cramer's rule 또는 가감법을 이용한 x, y 산출
-    double userX = (C * E - F * B) / (A * E - D * B);
-    double userY = (A * F - D * C) / (A * E - D * B);
-
-    return new double[]{userX, userY};
-  }
 }
